a0124d261e19e487e896712250206d0a8e7d4da8
mplumer <maxplumer12@gmail.com>
2022-11-16 11:19:20 -0600

    WHERE vs HAVING

--- 10-more-queries.sql
+++ 10-more-queries.sql
@@ -1,4 +1,15 @@
 -- SELECT * FROM users
 -- WHERE first_name LIKE 'Ma%';
 
-SELECT COUNT(id) FROM locations;
+-- SELECT COUNT(id) FROM locations;
+
+-- SELECT c.name, loc.street, COUNT(loc.id) FROM cities AS c
+-- LEFT JOIN locations AS loc ON loc.city_name = c.name
+-- GROUP BY c.name, loc.street;
+
+SELECT c.name, COUNT(loc.id) AS num_locations FROM cities AS c
+LEFT JOIN locations AS loc ON loc.city_name = c.name
+-- WHERE c.name = 'Berlin' OR c.name = 'Munich'
+GROUP BY c.name
+HAVING COUNT(loc.id) > 1
+;
